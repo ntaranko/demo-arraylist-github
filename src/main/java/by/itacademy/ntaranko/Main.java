a39864f51b8718029490884554e15a24fb16db91
ntaranko <ntaranko@gmail.com>
2024-12-23 20:55:21 +0300

    Initial commit

---
+++ src/main/java/by/itacademy/ntaranko/Main.java
@@ -0,0 +1,15 @@
+package by.itacademy.ntaranko;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Minsk");
+        cities.add("Brest");
+        cities.add("Moscow");
+        cities.add("New York");
+        cities.add("Ghomel");
+        System.out.println(cities);
+    }
+}
